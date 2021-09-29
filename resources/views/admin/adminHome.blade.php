69a2708463368929f67c2e54ce706b4bf68fc01e
MIdtor Jya <yaya17072541>
2021-09-30 06:41:09 +0700

    route admin v1

--- resources/views/admin/adminHome.blade.php
+++ resources/views/admin/adminHome.blade.php
@@ -1,113 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="container">
     <div class="row justify-content-center">
-     
+    
+            <div class="card">
+                <div class="card-header">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
 
-                <div class="card-body">
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    
+      <li class="nav-item">
+        <a class="nav-link" href="QuranVideos.php">จัดการกุรอาน</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="hometest.php">จัดารผู้ใช้</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login-form.html">จัดการตัฟซัร</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login-form.html">จัดการโน้ต</a>
+      </li>
+   
+    </ul>
+</nav>
+</div>
+
+<div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 </div>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Learn AdminLTE</title>
-
-    <link rel="stylesheet" href="{{ URL::to('adminassets/css/bootstrap.min.css') }}">
-    <!-- <link rel="stylesheet" href="css/fontawesome.min.css"> -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{ URL::to('adminassets/css/ionicons.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::to('adminassets/css/adminlte.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::to('adminassets/css/skin.min.css') }}">
-</head>
-<body class="skin-blue">
-
-
-    <div class="wrapper">
-
-        <!-- Main Header -->
-        <header class="main-header">
-      
-          <!-- Logo -->
-        
-      
-          <!-- Header Navbar -->
-          <nav class="navbar navbar-static-top" role="navigation">
-            <!-- Sidebar toggle button-->
-            
-            <!-- Navbar Right Menu -->
-           
-          </nav>
-        </header>
-        <!-- Left side column. contains the logo and sidebar -->
-        <aside class="main-sidebar">
-      
-          <!-- sidebar: style can be found in sidebar.less -->
-          <section class="sidebar">
-      
-            <!-- Sidebar user panel (optional) -->
-            <div class="user-panel">
-              <div class="pull-left image">
-                <img src="{{ URL::to('adminassets/img/profile.jpeg') }}" class="img-circle" alt="User Image">
-              </div>
-              <div class="pull-left info">
-                <p>Admin</p>
-                <!-- Status -->
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-              </div>
-            </div>
-      
-            <!-- search form (Optional) -->
-            <form action="#" method="get" class="sidebar-form">
-              <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search...">
-                <span class="input-group-btn">
-                    <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                    </button>
-                  </span>
-              </div>
-            </form>
-            <!-- /.search form -->
-      
-            <!-- Sidebar Menu -->
-            <ul class="sidebar-menu" data-widget="tree">
-              <li class="header">HEADER</li>
-              <!-- Optionally, you can add icons to the links -->
-              <li class="active"><a href="crud-user/index.php"><i class="fa fa-link"></i> <span>จัดการคำแปลกุรอาน</span></a></li>
-              <li><a href="m.users.php"><i class="fa fa-link"></i> <span>จัดการผู้ใช้</span></a></li>
-              <li><a href="index.html"><i class="fa fa-link"></i> <span>จัดการกุรอานตัฟซีร</span></a></li>
-              <li><a href="index.html"><i class="fa fa-link"></i> <span>จัดการโน้ต</span></a></li>
-              <!-- <li class="treeview">
-                <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
-                  <span class="pull-right-container">
-                      <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-                </a>
-                <ul class="treeview-menu">
-                  <li><a href="#">Link in level 2</a></li>
-                  <li><a href="#">Link in level 2</a></li>
-                </ul>
-              </li>
-            </ul>-->
-            <!-- /.sidebar-menu -->
-          </section>
-          <!-- /.sidebar -->
-        </aside>
-      
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-          <!-- Content Header (Page header) -->
-          <h1 class="mt-5">Infomation User</h1>
-          <br>
-
-
 <div class ="container">
   <table class="table table-hover table-bordered border-success table-striped">
   
@@ -1380,121 +1314,7 @@
   <br>
   <br>
    	</div>
-
-
-                  </div>
-              </div>
-      
-          </section>
-          <!-- /.content -->
-        </div>
-        <!-- /.content-wrapper -->
-      
-        <!-- Main Footer -->
-        <footer class="main-footer">
-          <!-- To the right -->
-          <div class="pull-right hidden-xs">
-          <strong>Quran.th &copy; 2021 <a href="#"></a></strong>
-          </div>
-          <!-- Default to the left -->
-         
-        </footer>
-      
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-          <!-- Create the tabs -->
-          <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-            <li class="active"><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-            <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-          </ul>
-          <!-- Tab panes -->
-          <div class="tab-content">
-            <!-- Home tab content -->
-            <div class="tab-pane active" id="control-sidebar-home-tab">
-              <h3 class="control-sidebar-heading">Recent Activity</h3>
-              <ul class="control-sidebar-menu">
-                <li>
-                  <a href="javascript:;">
-                    <i class="menu-icon fa fa-birthday-cake bg-red"></i>
-      
-                    <div class="menu-info">
-                      <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-      
-                      <p>Will be 23 on April 24th</p>
-                    </div>
-                  </a>
-                </li>
-              </ul>
-              <!-- /.control-sidebar-menu -->
-      
-              <h3 class="control-sidebar-heading">Tasks Progress</h3>
-              <ul class="control-sidebar-menu">
-                <li>
-                  <a href="javascript:;">
-                    <h4 class="control-sidebar-subheading">
-                      Custom Template Design
-                      <span class="pull-right-container">
-                          <span class="label label-danger pull-right">70%</span>
-                        </span>
-                    </h4>
-      
-                    <div class="progress progress-xxs">
-                      <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-                    </div>
-                  </a>
-                </li>
-              </ul>
-              <!-- /.control-sidebar-menu -->
-      
-            </div>
-            <!-- /.tab-pane -->
-            <!-- Stats tab content -->
-            <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
-            <!-- /.tab-pane -->
-            <!-- Settings tab content -->
-            <div class="tab-pane" id="control-sidebar-settings-tab">
-              <form method="post">
-                <h3 class="control-sidebar-heading">General Settings</h3>
-      
-                <div class="form-group">
-                  <label class="control-sidebar-subheading">
-                    Report panel usage
-                    <input type="checkbox" class="pull-right" checked>
-                  </label>
-      
-                  <p>
-                    Some information about this general settings option
-                  </p>
-                </div>
-                <!-- /.form-group -->
-              </form>
-            </div>
-            <!-- /.tab-pane -->
-          </div>
-        </aside>
-        <!-- /.control-sidebar -->
-        <!-- Add the sidebar's background. This div must be placed
-        immediately after the control sidebar -->
-        <div class="control-sidebar-bg"></div>
-      </div>
-      <!-- ./wrapper -->
-      
-    
-
-    <script src="{{ URL::to('adminassets/js/jquery.min.js') }}"></script>
-    <script src="{{ URL::to('adminassets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ URL::to('adminassets/js/adminlte.min.js') }}"></script>
-    <script>
-        $('#myBox').boxWidget({
-            animationSpeed: 500,
-            collapseTrigger: '#boxBtn',
-            removeTrigger: '#my-remove-button-trigger',
-            collapseIcon: 'fa-minus',
-            expandIcon: 'fa-plus',
-            removeIcon: 'fa-times'
-        })
-    </script>
-</body>
-</html>
-
 @endsection
+
+
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,9 +26,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+              <!-- Right Side Of Navbar  <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Quran.th') }}
-                </a>
+                </a> --> 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -77,10 +77,21 @@
                 </div>
             </div>
         </nav>
-
-        <main class="py-4">
+        
+        {{-- @yield('nav') --}}
+            
             @yield('content')
-        </main>
-    </div>
+   
+        
+		<!-- /Header -->
+		<!-- Sidebar -->
+	
+		<!-- /Sidebar -->
+		<!-- Page Wrapper -->
+		
+		<!-- /Page Wrapper -->
+	</div>
+
+ 
 </body>
 </html>
